4fe25fb2689cfa50fbc7ae75846599fc4cbd0e5f
danljs <daniel.liu1973@gmail.com>
2016-09-13 23:38:35 -0400

    ss

---
+++ src/main/java/point/data/CustomerRepository.java
@@ -0,0 +1,10 @@
+package point.data;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+}
